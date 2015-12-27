8cb9d483c0ad59002de0778c7a4d8deb4d2194fd
dcarnino <deniscarnino@gmail.com>
2015-12-27 20:09:28 +0100

    First commit.

---
+++ synthetic_signal.py
@@ -0,0 +1,17 @@
+##### Libraries #####
+import numpy as np
+import networkx as nx
+
+##### Classes #####
+### Class describing a synthetic signal
+class SyntheticSignal:
+
+    def __init__(self, nb_nodes, theta, kappa):
+        pass
+
+
+
+##### Main #####
+if __name__ == "__main__":
+    nb_nodes, theta, kappa = 100, 0.9, 0.5
+    synth_graph = SyntheticSignal(nb_nodes, theta, kappa)
